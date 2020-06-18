258d790afd3659578ae164b1b98f8e5821c0ac1d
shadhazi <sandor.hadhazi@qualysoft.com>
2020-06-18 16:34:01 +0200

    Initial project structure

---
+++ ToDoApp/ToDo.Con/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDo.Con
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
