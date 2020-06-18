0af7dabe7312b3ad3df86ef19cf48d42d412f43a
shadhazi <sandor.hadhazi@qualysoft.com>
2020-06-18 17:04:13 +0200

    Implementing Service

---
+++ ToDoApp/ToDo.Service/ToDoService.cs
@@ -0,0 +1,20 @@
+﻿using ToDo.Extensibility;
+using ToDo.Extensibility.Dto;
+
+namespace ToDo.Service
+{
+    public class ToDoService : IToDoService
+    {
+        private readonly IToDoRepository toDoRepository;
+
+        public ToDoService(IToDoRepository toDoRepository)
+        {
+            this.toDoRepository = toDoRepository;
+        }
+
+        public int CreateToDoItem(ToDoDto toDoDto)
+        {
+            return toDoRepository.Create(toDoDto);
+        }
+    }
+}
