574e1f0f536203010dbec92e387cd459a4fc4f59
shadhazi <sandor.hadhazi@qualysoft.com>
2020-06-19 12:33:11 +0200

    Added ninject bindings

---
+++ ToDoApp/ToDo.Domain/DomainNinjectModule.cs
@@ -0,0 +1,16 @@
+﻿using Ninject.Modules;
+using ToDo.Domain.Converters;
+using ToDo.Domain.Repositories;
+using ToDo.Extensibility;
+
+namespace ToDo.Domain
+{
+    public class DomainNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IToDoEntityConverter>().To<ToDoEntityConverter>();
+            Bind<IToDoRepository>().To<ToDoRepository>();
+        }
+    }
+}
---
+++ ToDoApp/ToDo.Service/ServiceNinjectModule.cs
@@ -0,0 +1,13 @@
+﻿using Ninject.Modules;
+using ToDo.Extensibility;
+
+namespace ToDo.Service
+{
+    public class ServiceNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IToDoService>().To<ToDoService>();
+        }
+    }
+}
