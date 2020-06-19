b3d66e7fd3f1ae39c859eb6fb99bbe5889a825c0
shadhazi <sandor.hadhazi@qualysoft.com>
2020-06-19 17:10:15 +0200

    Added db migration

--- ToDoApp/ToDo.Domain/Database/Model/ToDoDbModel.cs
+++ ToDoApp/ToDo.Domain/Database/Model/ToDoDbModel.cs
@@ -9,8 +9,11 @@ namespace ToDo.Domain.Database.Model
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(30)]
         public string Description { get; set; }
 
+        [Required]
         public bool IsCompleted { get; set; }
     }
 }
---
+++ ToDoApp/ToDo.Domain/Migrations/20200619145854_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDo.Domain.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(maxLength: 30, nullable: false),
+                    IsCompleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
