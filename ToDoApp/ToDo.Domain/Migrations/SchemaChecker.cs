62a6f6c1cab3eb7d9e3cfc1b87674c85d118a8ae
san-had <hadhazis@gmail.com>
2020-07-12 18:32:55 +0200

    Check schema at startup

---
+++ ToDoApp/ToDo.Domain/Migrations/SchemaChecker.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ToDo.Domain.Database.Providers;
+
+namespace ToDo.Domain.Migrations
+{
+    public static class SchemaChecker
+    {
+        public static void UpdateSchema(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<MsSqlLiteDatabaseContext>();
+                if (!context.Database.EnsureCreated())
+                {
+                    context.Database.Migrate();
+                }
+            }
+        }
+    }
+}
--- ToDoApp/ToDo.UI/Startup.cs
+++ ToDoApp/ToDo.UI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDo.Domain.Migrations;
 using ToDo.Extensibility.Dto;
 
 namespace ToDo.UI
@@ -45,6 +46,8 @@ namespace ToDo.UI
                     name: "default",
                     template: "{controller=ToDo}/{action=Index}/{id?}");
             });
+
+            app.UpdateSchema();
         }
     }
 }
